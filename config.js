fcdcdfdb7d67aa2fa2357f9c08f93c57441ef3cf
𝗧𝗛𝗔𝗥𝗨𝗦𝗛𝗔 𝗦𝗔𝗡𝗗𝗜𝗣𝗔 <166241288+mageekabn@users.noreply.github.com>
2024-11-02 07:51:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "Alive image url එකක් දමන්න",
-ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now❤️‍🩹😺"
+ALIVE_MS: process.env.ALIVE_MSG || "I'm Tharusha🍀❤️‍🩹,I'm Alive Now❤️‍🩹😺",
 };
